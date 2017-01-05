7d0781d2fa013c525bbe4fb07c609ac7eca74a7f
Fernando Gouvea <fernandosrg1@gmail.com>
2017-01-05 18:10:32 -0200

    Removendo print desncessáio no onService().

--- FCFS.py
+++ FCFS.py
@@ -10,7 +10,6 @@ class FCFS(Queue):
         self.queue.append(client)
     
     def onService(self):
-        print "Service", self.time
         if self.queue:
             next_client = self.queue.pop(0)
             self.current = next_client
