7ce6a2e8b7d15d0fe51d75ac117fedade51fa95b
Fernando Gouvea <fernandosrg1@gmail.com>
2017-01-05 18:52:39 -0200

    Retornando cliente atual para fila em vez de criar outro.

--- PreemptiveFCFS.py
+++ PreemptiveFCFS.py
@@ -2,24 +2,23 @@ from Queue import *
 from Client import Client
 
 class PreemptiveFCFS(Queue):
-    
+
     def __init__(self):
         Queue.__init__(self)
         self.queue1 = []
         self.queue2 = []
-    
+
     def adToQueue(self, client):
         if client.clazz == 1:
             if self.current.clazz == 1:
                 self.queue1.append(client)
             else:
-                lastClient = Client(self.current.id, self.current.time, self.current.residualTime, self.current.clazz)
-                self.queue2.insert(0, lastClient)
+                self.queue2.insert(0, self.current)
                 self.current = client
         else:
             self.queue2.append(client)
 
-    
+
     def serveNextClient(self):
         if self.queue1:
             next_client = self.queue1.pop(0)
@@ -27,6 +26,6 @@ class PreemptiveFCFS(Queue):
         elif self.queue2:
             next_client = self.queue2.pop(0)
             self.current = next_client
-            
+
     def getAllClients(self):
         return self.queue1 + self.queue2
--- PreemptiveLCFS.py
+++ PreemptiveLCFS.py
@@ -2,20 +2,19 @@ from Queue import *
 from Client import Client
 
 class PreemptiveLCFS(Queue):
-    
+
     def __init__(self):
         Queue.__init__(self)
         self.queue = []
-    
+
     def adToQueue(self, client):
-        lastClient = Client(self.current.id, self.current.time, self.current.residualTime, self.current.clazz)
-        self.queue.append(lastClient)
+        self.queue.append(self.current)
         self.current = client
-    
+
     def serveNextClient(self):
         if self.queue:
             next_client = self.queue.pop(len(self.queue)-1)
             self.current = next_client
-            
+
     def getAllClients(self):
         return self.queue
